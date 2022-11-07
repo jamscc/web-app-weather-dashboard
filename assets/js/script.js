e9e2a942c09c7358433b46fd8db333c072d1c0b6
J <jamscc22@gmail.com>
2022-11-06 19:53:00 -0500

    functions - to get and display forecast info

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,6 +68,10 @@ var keyOpenW = "4b473443b5c6795af3c6b075e5849721";
 var startingDate;
 var convertedTZone;
 
+// for forecast data
+var lat;
+var long;
+
 // get data
 function getData() {
 
@@ -143,11 +147,81 @@ function dataCurrentDisplay(data, citySearchName) {
 
         // weather icon
         var todaysWeatherIcon = data.weather[0].icon;
-        cityDateHeading.append($("<img id='today-weather-icon' src='http://openweathermap.org/img/wn/" + todaysWeatherIcon + "@2x.png'>"));
+        cityDateHeading.append($("<img id='today-weather-icon' src='https://openweathermap.org/img/wn/" + todaysWeatherIcon + "@2x.png'>"));
 
         // temperature, wind speed, and humidity
         infoTemp.text("Temperature: " + Math.round(data.main.temp) + "\u2109");
         infoWind.text("Wind Speed: " + Math.round(data.wind.speed) + " MPH");
         infoHumidity.text("Humidity: " + Math.round(data.main.humidity) + "%");
+
+        // coordinates to get forecast data
+        lat = data.coord.lat;
+        long = data.coord.lon;
+        dataForecast(lat, long);
     };
 }
+
+// get forecast data using coordinates
+function dataForecast(lat, long) {
+
+    var apiForecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&units=imperial" + "&appid=" + keyOpenW;
+
+    // get and display forecast data
+    fetch(apiForecastURL)
+        .then((response) => {
+            if (response.ok) {
+                response.json().then((data) => { dataForecastDisplay(data) });
+            } else {
+                messageOutput.text(response.statusText).attr('style', 'background: red; width: 75%');
+                searchInput.val('');
+                return;
+            }
+        })
+        .catch((error) => {
+            messageOutput.text(error).attr('style', 'background: red; width: 75%');
+            searchInput.val('');
+            return;
+        });
+}
+
+// display forecast data
+function dataForecastDisplay(data) {
+    divForecastDays.empty();
+
+    if (!data.list.length || !data) {
+        messageOutput.text("no data is available").attr('style', 'background: red; width: 75%');
+        searchInput.val('');
+        return;
+    };
+
+    var dayOneIndex = 7;
+    var dayTwoIndex = 15;
+    var dayThreeIndex = 23;
+    var dayFourIndex = 31;
+    var dayFiveIndex = 39;
+
+    var indexArray = [];
+    indexArray.push(dayOneIndex, dayTwoIndex, dayThreeIndex, dayFourIndex, dayFiveIndex);
+
+    // display forecast info using the index numbers above
+    for (var i = 0; i < indexArray.length; i++) {
+
+        var eachForecastDay = $("<div class='col-6 col-md-3 m-2 card border-primary text-primary five-days'>");
+        divForecastDays.append(eachForecastDay);
+
+        // future date
+        var dateFuture = moment.unix(data.list[indexArray[i]].dt).utc().utcOffset(convertedTZone).format('MMM DD, YYYY');
+
+        eachForecastDay.append($("<p class='font-weight-bold mt-2 h5 text-body'>" + dateFuture + "</p>"));
+
+        // weather icon
+        var iconInfo = data.list[indexArray[i]].weather[0].icon;
+
+        eachForecastDay.append($("<img id='icons-weather' src='https://openweathermap.org/img/wn/" + iconInfo + "@2x.png'>"));
+
+        // temperature, wind speed, and humidity
+        eachForecastDay.append($("<p class='text-body m-0 forecast-data'> T: " + Math.round(data.list[indexArray[i]].main.temp) + "\u2109" + '</p>'));
+        eachForecastDay.append($("<p class='text-body m-0 forecast-data'> W: " + Math.round(data.list[indexArray[i]].wind.speed) + " MPH" + "</p>"));
+        eachForecastDay.append($("<p class='text-body m-0 forecast-data'> H: " + Math.round(data.list[indexArray[i]].main.humidity) + "%" + "</p>"));
+    };
+};
